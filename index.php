213da68db85f5da0eb3836c5206677a2fd6d60e4
manos <manos@manos-Dell-System-XPS-L702X>
2013-07-28 00:01:33 +0200

    sample index

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+$dsn = "pgsql:"
+			. "host=ec2-54-227-238-31.compute-1.amazonaws.com;"
+    		. "dbname=d3r468400g680j;"
+    		. "user=wzcdebwgjfehyz;"
+    		. "port=5432;"
+    		. "sslmode=require;"
+    		. "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7";
+	$db = new PDO($dsn);
+ 
+
+?>
+
+<html>
+ <head>
+  <title>Employees</title>
+ </head>
+ <body>
+  <table>
+   <thead>
+    <tr>
+     <th>Employee ID</th>
+     <th>Last Name</th>
+     <th>First Name</th>
+     <th>Title</th>
+    </tr>
+   </thead>
+   <tbody>
+<?php
+$query = "SELECT employee_id, last_name, first_name, title "
+     . "FROM employees ORDER BY last_name ASC, first_name ASC";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["employee_id"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
+    echo "</tr>";
+}
+$result->closeCursor();
+?>
+   </tbody>
+  </table>
+ </body>
+</html>
