cf19c5cd75a2b899cc797cf88c8cf6e61271154a
manos <manos@manos-Dell-System-XPS-L702X>
2013-08-30 15:59:33 +0200

    users

--- api/users.php
+++ api/users.php
@@ -82,12 +82,14 @@ oid desc limit 1; */
 			$user_array['entity']          = $row['entity'];
 		}
 		
-		$res = $app->response();
+		//$res = $app->response();
 		
-		$res['Content-Type'] = 'application/json';
-		$res['X-Powered-By'] = 'Slim';
+		//$res['Content-Type'] = 'application/json';
+		//$res['X-Powered-By'] = 'Slim';
 		
-		echo json_encode($user_array);
+		$app->response->headers->set('Content-Type', 'application/json');		
+		$app->response->setBody(json_encode($user_array));
+		//echo json_encode($user_array);
 		
 			
 		
