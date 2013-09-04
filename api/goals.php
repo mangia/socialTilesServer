34e2940d9e81842c30964ceaa3ff87db1eb96a4a
manos <manos@manos-Dell-System-XPS-L702X>
2013-09-04 14:35:14 +0200

    goals

--- api/goals.php
+++ api/goals.php
@@ -1,80 +1,83 @@
 <?php
-	require 'Slim/Slim.php';
-	require 'tags.php';
-	
-	\Slim\Slim::registerAutoloader();
+require 'Slim/Slim.php';
+require 'tags.php';
 
-	$app = new \Slim\Slim();
+\Slim\Slim::registerAutoloader();
 
-	$dsn = "pgsql:"
-			. "host=ec2-54-227-238-31.compute-1.amazonaws.com;"
-    		. "dbname=d3r468400g680j;"
-    		. "user=wzcdebwgjfehyz;"
-    		. "port=5432;"
-    		. "sslmode=require;"
-    		. "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7 ";
-	$db = new PDO($dsn); 
-	
-	if($app->request()->isGet()){
-		if($app->request()->get(Tags::$op) == "created_for"){
-			$entity =  $app->request()->get(Tags::$entity);	
+$app = new \Slim\Slim();
 
-			$query = "SELECT *  ".
-			"  FROM goals WHERE  created_for =".$entity." ;";
-			//echo $query;			
-			$result = $db->query($query);
-			echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-		}		
-		else if ($app->request()->get(Tags::$op) == "achieved_by"){		
-			$user_id =$app->request()->get(Tags::$user_id);
-			$query = "SELECT *  FROM goals WHERE achieved_by=".$goal_id.";";
-			$result = $db->query($query);
-			//echo $query;
-			echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-		}
-	}
-	if($app->request()->isPost()){
-		
+$dsn = "pgsql:" . "host=ec2-54-227-238-31.compute-1.amazonaws.com;" . "dbname=d3r468400g680j;" . "user=wzcdebwgjfehyz;" . "port=5432;" . "sslmode=require;" . "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7 ";
+$db = new PDO($dsn);
 
-		$name               	= $app->request()->post('name');
-		$goal_type				= $app->request()->post('goal_type');
-		$threshold				= $app->request()->post('threshold');
-    	$reward_points     	= $app->request()->post('reward_points');
-		$created_for			= $app->request()->post('creator');
-		$achieved_by			= $app->request()->post('achieved_by');
-		$start_date        	= $app->request()->post('start_date');
-		$end_date           = $app->request()->post('end_date');		
-		$date               	= $app->request()->post('date_created');
-		
-		
-		$query = "INSERT INTO entities(type) SELECT ".Tags::$newEvent.";";
-		$result = $db->query($query);
-		
-		$query = "SELECT id FROM  entities ORDER BY id desc limit 1 ; "	;
-		$result = $db->query($query);
-		
-		$row = $result->fetch(PDO::FETCH_ASSOC);
-		$entity_id = $row['id'];	
-		
-		$query ="INSERT INTO goals (name, goal_type, threshold, reward_points, created_for, achieved_by, start_date, end_date, date_created)	SELECT ".
-		" ".$name.",".
-		" ".$goal_type.",".
-		" ".$threshold.",".
-		" ".$reward_points.", ".
-		" ".$created_for.",".
-		" ".$achieved_by."'".
-		" '".$start_date."',".
-		" '".$end_date."',".
-		" '".$date."';";
-		
-		//echo "querry is: ".$query;
-		$result = $db->query($query);
-		
-		$query = "SELECT *  FROM goals ORDER BY goal_id desc limit 1 ; "	;
-		$result = $db->query($query);
-		$row = $result->fetch(PDO::FETCH_ASSOC);	
-				
-		echo json_encode($row);
-		
-	}
+if ($app -> request() -> isGet()) {
+    if ($app -> request() -> get(Tags::$op) == "created_for") {
+        $entity = $app -> request() -> get(Tags::$entity);
+
+        $query = "SELECT *  " . "  FROM goals WHERE  created_for =" . $entity . " ;";
+        //echo $query;
+        $result = $db -> query($query);
+        echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
+    } else if ($app -> request() -> get(Tags::$op) == "achieved_by") {
+        $user_id = $app -> request() -> get(Tags::$user_id);
+        $query = "SELECT *  FROM goals WHERE achieved_by=" . $goal_id . ";";
+        $result = $db -> query($query);
+        //echo $query;
+        echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
+    }
+}
+if ($app -> request() -> isPost()) {
+
+    $name = $app -> request() -> post('name');
+    $goal_type = $app -> request() -> post('goal_type');
+    $threshold = $app -> request() -> post('threshold');
+    $reward_points = $app -> request() -> post('reward_points');
+    $achieved_by = $app -> request() -> post('achieved_by');
+    $start_date = $app -> request() -> post('start_date');
+    $end_date = $app -> request() -> post('end_date');
+    $date = $app -> request() -> post('date_created');
+
+    if ($app -> request() -> post(Tags::$op) == "single")  {
+        $created_for = $app -> request() -> post('creator');
+        $query = "INSERT INTO goals (name, goal_type, threshold, reward_points, created_for, achieved_by, start_date, end_date, date_created)   SELECT " 
+        ." ". $name ."," 
+        ." ". $goal_type ."," 
+        . " ". $threshold ."," 
+        ." ". $reward_points .", " 
+        ." ". $created_for ."," 
+        ." ". $achieved_by ."'" 
+        ." '". $start_date ."'," 
+        ." '". $end_date ."'," 
+        ." '". $date ."';";
+
+        //echo "querry is: ".$query;
+        $result = $db -> query($query);
+
+        $query = "SELECT *  FROM goals ORDER BY goal_id desc limit 1 ; ";
+        $result = $db -> query($query);
+        $row = $result -> fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($row);
+     } else if ($app -> request() -> post(Tags::$op) == "multiple") {
+        $created_for_multi = $app->request()->post('created_for_multi');
+        $created_for_array = json_decode($created_for_multi);
+        $query = "INSERT INTO goals (name, goal_type, threshold, reward_points, created_for, achieved_by, start_date, end_date, date_created)   VALUES";
+        foreach ($created_for_array  as $i => $value) {
+            $query .= "( ". $name ."," 
+            ." ". $goal_type ."," 
+            . " ". $threshold ."," 
+            ." ". $reward_points .", " 
+            ." ". $created_for ."," 
+            ." ". $achieved_by ."'" 
+            ." '". $start_date ."'," 
+            ." '". $end_date ."'," 
+            ." '". $date ."'),";
+            
+        }
+        $query = substr($query, 0, -1);
+        $query .= " ;";
+         //echo $query;
+        $result = $db->query($query);
+    }
+
+}
 ?>
--- api/group_members.php
+++ api/group_members.php
@@ -1,90 +1,57 @@
 <?php
-	require 'Slim/Slim.php';
-	require 'tags.php';
-	
-	\Slim\Slim::registerAutoloader();
+require 'Slim/Slim.php';
+require 'tags.php';
 
-	$app = new \Slim\Slim();
-	
-	
-	$dsn = "pgsql:"
-			. "host=ec2-54-227-238-31.compute-1.amazonaws.com;"
-    		. "dbname=d3r468400g680j;"
-    		. "user=wzcdebwgjfehyz;"
-    		. "port=5432;"
-    		. "sslmode=require;"
-    		. "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7 ";
-	$db = new PDO($dsn); 
-	
-	if($app->request()->isGet()){
-		
-		$group	= $app->request()->get(Tags::$group_id);
-		
-		//echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));		
-				
-		if($app->request()->get(Tags::$op) == Tags::$user){
-			$query = "SELECT *  FROM users WHERE ";			
-			$flag = true;			
-			//while ($row = $result->fetch(PDO::FETCH_ASSOC)){
-				//echo json_encode($row);
-				/*if(!$flag){
-					$query .= " OR ";
-				}
-				else{
-					$flag = false;			
-				}
-				
-				$query .= " user_id =".$row[Tags::$user_id]."";			
-			}
-			
-			$query.=";";
-			//echo $query;
-			$result = $db->query($query);
-			
-			echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));*/
-			$query = "SELECT DISTINCT ON (u.user_id) * FROM group_members gm, users u WHERE gm.group_id='".$group."' AND gm.status = 1 AND gm.user_id = u.user_id";
-			//echo $query;
-						
-			$result = $db->query($query);			
-			echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-		}
-		else{
-			$query = "SELECT *  FROM group_members gm WHERE gm.group_id='".$group."' AND gm.status = 1";
-			$result = $db->query($query);		
-			echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));	
-		}
+\Slim\Slim::registerAutoloader();
 
-		
-	}
-	if($app->request()->isPost()){
-		if($app->request()->post(Tags::$op) == "sinngle"){
-			$user  	= $app->request()->post(Tags::$user_id);
-			$group	= $app->request()->post(Tags::$group_id);	
-			$query = "INSERT INTO group_members(group_id, user_id, status) VALUES (".
-			" ".$group.", ".
-			" ".$user.", 0 );";		
-			$result = $db->query($query);
-		}
-		else if($app->request()->post(Tags::$op) == "multiple"){
-			$user_idss = $app->request()->post(Tags::$user_ids) ;
-			$group	= $app->request()->post(Tags::$group_id);
-			$user_ids = json_decode($user_idss, true);
-			//var_dump($user_ids); 
-			//echo $user_ids;
+$app = new \Slim\Slim();
 
-			foreach ($user_ids as $i => $value){
-				//echo $user_ids[$i];
-				$query = "INSERT INTO group_members(group_id, user_id, status) VALUES (".
-			" ".$group.", ".
-			" ". $user_ids[$i].", 0 );";		
-			$result = $db->query($query);
-			}
-			
-		}
-		echo "ok";
-	}
+$dsn = "pgsql:" . "host=ec2-54-227-238-31.compute-1.amazonaws.com;" . "dbname=d3r468400g680j;" . "user=wzcdebwgjfehyz;" . "port=5432;" . "sslmode=require;" . "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7 ";
+$db = new PDO($dsn);
 
+if ($app -> request() -> isGet()) {
+
+    $group = $app -> request() -> get(Tags::$group_id);
+
+    //echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+
+    if ($app -> request() -> get(Tags::$op) == Tags::$user) {
+        $query = "SELECT *  FROM users WHERE ";
+        $flag = true;
+
+        $query = "SELECT DISTINCT ON (u.user_id) * FROM group_members gm, users u WHERE gm.group_id='" . $group . "' AND gm.status = 1 AND gm.user_id = u.user_id";
+        //echo $query;
+
+        $result = $db -> query($query);
+        echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
+    } else {
+        $query = "SELECT *  FROM group_members gm WHERE gm.group_id='" . $group . "' AND gm.status = 1";
+        $result = $db -> query($query);
+        echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
+    }
+
+}
+if ($app -> request() -> isPost()) {
+    if ($app -> request() -> post(Tags::$op) == "single") {
+        $user = $app -> request() -> post(Tags::$user_id);
+        $group = $app -> request() -> post(Tags::$group_id);
+        $query = "INSERT INTO group_members(group_id, user_id, status) VALUES (" . " " . $group . ", " . " " . $user . ", 0 );";
+        $result = $db -> query($query);
+    } else if ($app -> request() -> post(Tags::$op) == "multiple") {
+        $user_idss = $app -> request() -> post(Tags::$user_ids);
+        $group = $app -> request() -> post(Tags::$group_id);
+        $user_ids = json_decode($user_idss, true);
+        //var_dump($user_ids);
+        //echo $user_ids;
+
+        foreach ($user_ids as $i => $value) {
+            //echo $user_ids[$i];
+            $query = "INSERT INTO group_members(group_id, user_id, status) VALUES (" . " " . $group . ", " . " " . $user_ids[$i] . ", 0 );";
+            $result = $db -> query($query);
+        }
+
+    }
+    echo "ok";
+}
 ?>
 
-
-	
