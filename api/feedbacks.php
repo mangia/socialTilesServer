e8a38226f7cb6cc52be735fbe5cd726cce9946f8
manos <manos@manos-Dell-System-XPS-L702X>
2013-09-06 19:53:00 +0200

    feedback

--- api/feedbacks.php
+++ api/feedbacks.php
@@ -13,7 +13,7 @@ if ($app -> request() -> isGet()) {
     $user_id = $app -> request() -> get(Tags::$user_id);
     $query = "SELECT * FROM feedback  WHERE user_id=" . $user_id . " ORDER BY date_created DESC ";
     $result = $db -> query($query);
-    echo $query ;
+    echo $query;
     echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
 }
 
@@ -29,23 +29,20 @@ if ($app -> request() -> isPost()) {
     $level = $app -> request() -> post('level');
     $size = $app -> request() -> post('size');
     $score = $app -> request() -> post('score');
-    
+
     $response_array = array();
-    
+
     //echo "Received post request";
 
-    $query = "INSERT INTO feedback (user_id, gamename, date_created, points, miss, duration, winner, level, size, score) SELECT " 
-        .$user_id." ,'".$gamename."','".$date."',".$points."," 
-        .$miss.",".$duration.",".$winner.",".$level.",".$size.","
-        .$score ." WHERE exists( SELECT * FROM users s WHERE s.user_id='".$user_id."');";
+    $query = "INSERT INTO feedback (user_id, gamename, date_created, points, miss, duration, winner, level, size, score) SELECT " . $user_id . " ,'" . $gamename . "','" . $date . "'," . $points . "," . $miss . "," . $duration . "," . $winner . "," . $level . "," . $size . "," . $score . " WHERE exists( SELECT * FROM users s WHERE s.user_id='" . $user_id . "');";
     //echo "querry is: ".$query;
     $result = $db -> query($query);
     //echo "Result is: ".$result;
-    
-    $query = "SELECT *  FROM feedback ORDER BY feedback_id desc limit 1 ; "  ;
-    $result = $db->query($query);
-    $response_array['feedback'] = $result->fetch(PDO::FETCH_ASSOC);    
-    
+
+    $query = "SELECT *  FROM feedback ORDER BY feedback_id desc limit 1 ; ";
+    $result = $db -> query($query);
+    $response_array['feedback'] = $result -> fetch(PDO::FETCH_ASSOC);
+
     $query = "UPDATE users SET total_score = total_score +" . $score . " ,total_duration = total_duration+" . $duration . " WHERE user_id='" . $user_id . "'";
     //echo "querry is: ".$query;
     $result = $db -> query($query);
@@ -72,7 +69,7 @@ if ($app -> request() -> isPost()) {
         $result = $db -> query($query);
         $response = "newEntry";
     }
-    
+
     $goals_array = array();
     $response_array['highscores'] = $response;
     $response_array['has_goals'] = FALSE;
@@ -87,14 +84,20 @@ if ($app -> request() -> isPost()) {
         $threshold = $row['threshold'];
         $currently = $row['currently'];
         $goal_gamename = $row['game_name'];
-        $is_finished = $row['is_finished'] ;
+        $is_finished = $row['is_finished'];
         $is_finished = 0;
+        
+        echo "game name is ".$gamename." and goal game name is ".$goal_gamename." and are they equal ? ".($gamename == $goal_gamename)."";
+        
         if ($goal_type == 2) {
+            echo "i am here in goal type 2";
             $currently = $currently + $score;
-            $query = "UPDATE goals SET highscore=" .  $currently . " WHERE achieved_by ='".$user_id." ;";
+            $query = "UPDATE goals SET highscore=" . $currently . " WHERE achieved_by ='" . $user_id . " ;";
             $result = $db -> query($query);
-        } else if($gamename == $goal_gamename){// event
 
+        } 
+        if ($gamename == $goal_gamename) {// event
+             echo "i am here in game names equaliti";
             if ($goal_type == 0) {// gather score
                 $currently = $currently + $score;
             } else if ($goal_type == 1) {//play for
@@ -107,16 +110,17 @@ if ($app -> request() -> isPost()) {
                 $row['is_finished'] = 1;
                 $goals_array[] = $row;
             }
-            
-            $query = "UPDATE goals SET highscore=" .  $currently . " WHERE achieved_by ='".$user_id." AND is_finished = ".$is_finished." ;";
+
+            $query = "UPDATE goals SET highscore=" . $currently . " WHERE achieved_by ='" . $user_id . " AND is_finished = " . $is_finished . " ;";
+            $result = $db -> query($query);
         }
 
     }
-    
-    if( $response_array['has_goals'] == TRUE){
+
+    if ($response_array['has_goals'] == TRUE) {
         $response_array['goals'] = $goals_array;
     }
-    
+
     echo json_encode($response_array);
 }
 ?>
