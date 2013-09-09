2eba5e1c426d86e58582662b09051a5a345f06fa
manos <manos@manos-Dell-System-XPS-L702X>
2013-09-09 05:46:19 +0200

    variety

---
+++ api/highscores.php
@@ -0,0 +1,18 @@
+<?php
+require 'Slim/Slim.php';
+require 'tags.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$dsn = "pgsql:" . "host=ec2-54-227-238-31.compute-1.amazonaws.com;" . "dbname=d3r468400g680j;" . "user=wzcdebwgjfehyz;" . "port=5432;" . "sslmode=require;" . "password=U2hPQsSC7_oM4bV-Fp7NiRy9j7 ";
+$db = new PDO($dsn);
+
+if ($app -> request() -> isGet()) {
+    $user_id = $app->request()->get(Tags::$user_id);
+    $query = "SELECT * FROM highscores WHERE  user_id = ".$user_id." ";
+    $result = $db -> query($query);
+    echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
+}
+?>
