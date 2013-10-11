7909b3d8c681835171e6d6d7cbb2162432c37099
manos <manos@manos-Dell-System-XPS-L702X>
2013-10-11 06:29:18 +0200

    different changes

--- api/feedbacks.php
+++ api/feedbacks.php
@@ -94,7 +94,7 @@ if ($app -> request() -> isPost()) {
         if ($goal_type == 2) {
             echo "i am here in goal type 2";
             $currently = $currently + $score;
-            $query = "UPDATE goals SET currently=" . $currently . " WHERE achieved_by ='" . $user_id . " ;";
+            $query = "UPDATE goals SET currently=" . $currently . " WHERE achieved_by =" . $user_id . " AND goal_id =".$goal_id.";";
             echo $query;
             $result = $db -> query($query);
 
